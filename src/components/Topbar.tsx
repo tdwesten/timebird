5c1b043d1674fc28b992d9b44f9d0d51de681890
Thomas van der Westen <thomas@codesmiths.nl>
2025-06-25 13:07:31 +0200

    Remove WebkitAppRegion styles from Topbar component for improved drag behavior

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,10 +3,10 @@ import { SettingsDialog } from "./SettingsDialog";
 
 export function Topbar() {
   return (
-    <div className="flex justify-between items-center p-2 border-b sticky top-0 bg-white z-40 select-none rounded-t-xl" style={{ WebkitAppRegion: 'drag' }}>
+    <div className="flex justify-between items-center p-2 border-b sticky top-0 bg-white z-40 select-none rounded-t-xl" >
       <Logo />
       <h1 className="text-xl font-semibold">TimeBird</h1>
-      <div style={{ WebkitAppRegion: 'no-drag' }}>
+      <div>
         <SettingsDialog />
       </div>
     </div>
